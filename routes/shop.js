edcc4757f054bfed0cb026dcfdc205316cf2d8ca
Sanchit <sanchitbhadgal36@gmail.com>
2021-12-26 09:56:48 +0530

    ejs template engine

--- routes/shop.js
+++ routes/shop.js
@@ -10,9 +10,6 @@ router.get("/", (req, res, next) => {
     prods: products,
     pageTitle: "Shop",
     path: "/",
-    hasProduct: products.length > 0,
-    activeShop: true,
-    productCSS: true,
   });
 });
 
